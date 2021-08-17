f1a1670750842d270ad0bd6fed6f9bc7137a5430
Alice Ratton <aliceratton@gmail.com>
2021-08-17 11:15:56 -0300

    updating navbar, links, and banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+   get 'about', to: 'pages#about', as: :about
+   get 'meal', to: 'pages#meal', as: :meal
+   get 'order', to: 'pages#order', as: :order
+   get 'galery', to: 'pages#galery', as: :galery
+   get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
